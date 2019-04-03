30a45a946ae2144003c6039b175e6ef7496d9754
Mack Hendricks <mack@dopensource.com>
2019-04-03 18:08:54 +0000

    Initial commit of a tf file that builds a machine for building docker images

---
+++ prod-env/build-server.tf
@@ -0,0 +1,31 @@
+variable "do_token" {}
+variable "dropletname" {
+    default="imagebuilder"
+}
+
+variable "number_of_servers" {}
+
+
+provider "digitalocean" {
+        token = "${var.do_token}"
+}
+
+
+
+data "digitalocean_ssh_key" "jump" {
+  name = "Jump"
+}
+
+resource "digitalocean_droplet" "dsiprouterDroplet" {
+        name = "${var.dropletname}-${count.index}"
+        count = "${var.number_of_servers}"
+        region = "nyc1"
+        size="1gb"
+        image="debian-9-x64"
+        ssh_keys = [ "${data.digitalocean_ssh_key.jump.fingerprint}" ]
+        
+}
+
+output "ip" {
+  value = "${digitalocean_droplet.dsiprouterDroplet.*.ipv4_address}"
+}
