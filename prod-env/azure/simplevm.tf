d0d82bf34fc484342549eb27863405cd20d27edc
Mack Hendricks <mack@dopensource.com>
2019-04-11 11:57:50 -0400

    Fixed error

--- prod-env/azure/simplevm.tf
+++ prod-env/azure/simplevm.tf
@@ -1,10 +1,3 @@
-# Define some variables
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" = {}
-
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     subscription_id = "${var.subscription_id}"
