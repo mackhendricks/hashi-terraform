585245ab576e725d2a5da81eb5c7377db7d349d9
Mack Hendricks <mack@dopensource.com>
2019-04-24 13:52:06 -0400

    Update simplevm.tf

--- prod-env/azure/simplevm.tf
+++ prod-env/azure/simplevm.tf
@@ -125,7 +125,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     location              = "eastus"
     resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
     network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
-    vm_size               = "Standard_D1_v2"
+    vm_size               = "Standard_XXM1_v2"
 
     storage_os_disk {
         name              = "myOsDisk"
